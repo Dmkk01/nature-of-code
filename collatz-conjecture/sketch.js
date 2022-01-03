2fbb961dd0e3aec5c88706ef69bb1427e09add97
Dominik <dmkk1@outlook.com>
2022-01-03 03:07:56 +0200

    Collatz conjecture

---
+++ collatz-conjecture/sketch.js
@@ -0,0 +1,56 @@
+let len = 10;
+let angle = 0.20;
+let frequency = 0.05;
+
+function setup() {
+    createCanvas(1200, 850);
+    background(0);
+
+    for (let i = 1; i < 10000; i++) {
+
+        let sequence = [];
+        let n = i;
+
+        while(n != 1) {
+            sequence.push(n);
+            n = collatz(n);
+        }
+
+        sequence.push(1);
+        sequence.reverse();
+
+        resetMatrix();
+        translate(width/1.8, 0);
+
+        for (let j = 0; j < sequence.length; j++) {
+            let value = sequence[j];
+            if (value % 2 == 0) {
+                rotate(angle);
+            } else {
+                rotate(-angle);
+            }
+            strokeWeight(2);
+            getColor(j)
+            
+
+            line(0, 0, 0, len);
+            translate(0, len);
+        }
+    }
+    saveCanvas('collatz-conjecture', 'jpg');
+  }
+  
+function collatz(n) {
+    if (n % 2 == 0) {
+        return n / 2;
+    } else {
+        return (n * 3 + 1) / 2;
+    }
+}
+
+function getColor(i) {
+    red   = sin(frequency*i ) * 127 + 128;
+    green = sin(frequency*i + 2*PI/3) * 127 + 128;
+    blue  = sin(frequency*i + 4*PI/3) * 127 + 128;
+    stroke(red, green, blue, 50);
+} 
